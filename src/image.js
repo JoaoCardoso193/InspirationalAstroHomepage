d578c5d16efa86411e06f9e88ed356dd9f69921b
Joao Cardoso <joao.cardoso@yale.edu>
2021-01-21 20:34:23 +0000

    added image alt

--- src/image.js
+++ src/image.js
@@ -1,6 +1,6 @@
 function image(props){
     return(
-        <img className='image' src={props.imgUrl}/>
+        <img className='image' src={props.imgUrl} alt="Astro image of the day, go down for its title and description"/>
     )
 }
 
